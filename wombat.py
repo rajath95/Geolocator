f06eb6160b7b64414b89d81e0ea172b50806b95c
rajath95 <simplytalk95@gmail.com>
2017-04-20 16:34:35 +0530

    second commit

---
+++ wombat.py
@@ -0,0 +1,28 @@
+import urllib
+import xml.etree.ElementTree as ET
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
+
+while True:
+    address = raw_input('Enter location: ')
+    if len(address) < 1 : break
+
+    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved',len(data),'characters'
+    print data
+    tree = ET.fromstring(data)
+
+
+    results = tree.findall('result')
+    print 'result =',results[0]
+    lat = results[0].find('geometry').find('location').find('lat').text
+    lng = results[0].find('geometry').find('location').find('lng').text
+    location = results[0].find('address_component').find('long_name').text
+
+    print 'lat',lat,'lng',lng
+    print location
+
+
